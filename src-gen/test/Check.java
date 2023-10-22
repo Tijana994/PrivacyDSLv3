ec421124eca1848d00f42aadee79d5dc50a86d49
Tijana994 <33682395+Tijana994@users.noreply.github.com>
2023-10-22 19:26:53 +0200

    Loading Factory

--- src-gen/test/Check.java
+++ src-gen/test/Check.java
@@ -1,11 +1,13 @@
 package test;
 
+import utility.PrivacyModelRepository;
 import utility.ProjectConfiguration.Configuration;
 
 public class Check {
 
 	public static void main(String[] args) {
 		Configuration.createDefaultConfiguration();
+		var test = new PrivacyModelRepository();
 		/*var validation = new Validation();
 		validation.checkValidation();*/
 	}
--- src-gen/utility/PrivacyModelRepository.java
+++ src-gen/utility/PrivacyModelRepository.java
@@ -9,42 +9,55 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
+import privacyModel.PrivacyModelFactory;
 import privacyModel.PrivacyModelPackage;
 import privacyModel.PrivacyPolicy;
 import utility.ProjectConfiguration.Configuration;
 
 public class PrivacyModelRepository {
 
+	public PrivacyModelRepository()
+	{
+		
+	}
+	
     public PrivacyPolicy getModel() {
     	PrivacyModelPackage.eINSTANCE.eClass();
         Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
         Map<String, Object> m = reg.getExtensionToFactoryMap();
         m.put("xmi", new XMIResourceFactoryImpl());
 
-        // Obtain a new resource set
-        ResourceSet resSet = new ResourceSetImpl();
-        registerPackage(resSet);
+	    // Obtain a new resource set
+	    ResourceSet resSet = new ResourceSetImpl();
+	    registerPackage(resSet);
+	
+	    // Get the resource
+	    Resource resource = resSet.getResource(URI.createURI(Configuration.getXmlPath()), true);
+	    PrivacyPolicy policy = (PrivacyPolicy)resource.getContents().get(0);
+	    return policy;
+	}
 
-        // Get the resource
-        Resource resource = resSet.getResource(URI.createURI(Configuration.getXmlPath()), true);
-        PrivacyPolicy policy = (PrivacyPolicy)resource.getContents().get(0);
-        return policy;
-    }
-    
-    public Boolean saveModel()
-    {
-    	//TODO
-    	return true;
-    }
-    
-    private void registerPackage(ResourceSet resSet) {
-        try {
-            Class<?> c = this.getClass().getClassLoader().loadClass("privacyModel.PrivacyModelPackage");
-            Field fNS = c.getField("eNS_URI");
-            Field fI = c.getField("eINSTANCE");
-            resSet.getPackageRegistry().put(fNS.get(null).toString(), fI.get(null));
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
+	public Boolean saveModel(){
+		//TODO
+		return true;
+	}
+	
+	public PrivacyModelFactory getFactory(){
+		var p = PrivacyModelPackage.eINSTANCE;
+	    // Retrieve the default factory singleton
+		return PrivacyModelFactory.eINSTANCE;
+	}
+	
+	private void registerPackage(ResourceSet resSet) {
+	    try {
+	        Class<?> c = this.getClass().getClassLoader().loadClass("privacyModel.PrivacyModelPackage");
+	        Field fNS = c.getField("eNS_URI");
+	        Field fI = c.getField("eINSTANCE");
+	        resSet.getPackageRegistry().put(fNS.get(null).toString(), fI.get(null));
+	    	} 
+	    catch (Exception e) {
+	        // TODO: handle exception
+	    	System.out.println("Cannot find class.");
+	        }
+	 }
 }
