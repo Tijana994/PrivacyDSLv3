b19d901ee2259d89bdc10a9de453bcf9613d25d0
Tijana994 <tijana.vdn@gmail.com>
2024-05-05 19:30:59 +0200

    Support for creating Shared Privacy Data with Privacy Data

--- src-gen/utility/PrivacyDataFactory.java
+++ src-gen/utility/PrivacyDataFactory.java
@@ -46,5 +46,28 @@ public class PrivacyDataFactory {
 			model.getAllSharedPrivacyData().add(sharedPrivacyDataObject);
 			repo.saveModel(model);
 		}
+		
+		public static void addPrivacyDataWithSharedPrivacyData(String name, DataType type,
+				Boolean collectedFromSubject, String privacyDataSource, List<String> additionalProtectionControls)
+		{
+			var repo = new PrivacyModelRepository();
+			var model = repo.getModel();
+			var privacyDataObject = repo.getFactory().createPrivacyData();
+			privacyDataObject.setName(name);
+			privacyDataObject.setType(type);
+			model.getAllDatas().add(privacyDataObject);
+			var sharedPrivacyDataObject = repo.getFactory().createSharedPrivacyData();
+			sharedPrivacyDataObject.setName(name);
+			sharedPrivacyDataObject.setCollectedFromSubject(collectedFromSubject);
+			sharedPrivacyDataObject.setDataSource(privacyDataSource);
+			sharedPrivacyDataObject.setPrivacydata(privacyDataObject);
+			if(additionalProtectionControls != null)
+			{
+				sharedPrivacyDataObject.getAdditionalProtectionControls().addAll(additionalProtectionControls);
+			}
+			
+			model.getAllSharedPrivacyData().add(sharedPrivacyDataObject);
+			repo.saveModel(model);
+		}
 	}
 }
