7ecb5974c114a02cd9afd89f55bf40a928a564f8
Tijana994 <tijana.vdn@gmail.com>
2024-10-29 10:03:11 +0100

    AddRegion and AddCity

--- src-gen/utility/PrivacyDataFactory.java
+++ src-gen/utility/PrivacyDataFactory.java
@@ -3,6 +3,7 @@ package utility;
 import java.util.List;
 
 import privacyModel.DataType;
+import privacyModel.LocationType;
 
 public class PrivacyDataFactory {
 
@@ -67,6 +68,40 @@ public class PrivacyDataFactory {
 			}
 			
 			model.getAllSharedPrivacyData().add(sharedPrivacyDataObject);
+			repo.saveModel(model);
+		}
+		
+		public static void addRegion(String name, String parentId)
+		{
+			var repo = new PrivacyModelRepository();
+			var model = repo.getModel();
+			var locationObject = repo.getFactory().createLocation();
+			locationObject.setName(name);
+			locationObject.setType(LocationType.REGION);
+			var parentObj = model.getLocations().stream().filter(location -> location.getName().equals(parentId)).findFirst();
+			if(parentObj.isPresent())
+			{
+				parentObj.get().getSubLocations().add(locationObject);
+			}
+			else
+			{
+				System.out.println("There is no location with id " + parentId);
+				return;
+			}
+			model.getLocations().add(locationObject);
+			repo.saveModel(model);
+		}
+		
+		public static void addCountry(String name, Boolean isEUmember, int legalAgeLimit)
+		{
+			var repo = new PrivacyModelRepository();
+			var model = repo.getModel();
+			var locationObject = repo.getFactory().createLocation();
+			locationObject.setName(name);
+			locationObject.setType(LocationType.COUNTRY);
+			locationObject.setIsEUMember(isEUmember);
+			locationObject.setLegalAgeLimit(legalAgeLimit);
+			model.getLocations().add(locationObject);
 			repo.saveModel(model);
 		}
 	}
