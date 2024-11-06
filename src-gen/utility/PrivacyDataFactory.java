61d9bce8e765a2975db424afa39f5b55429b91ef
Tijana994 <tijana.vdn@gmail.com>
2024-11-06 18:20:58 +0100

    addChildCustodyDocument helper

--- src-gen/utility/PrivacyDataFactory.java
+++ src-gen/utility/PrivacyDataFactory.java
@@ -1,9 +1,16 @@
 package utility;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
 import java.util.List;
 
 import privacyModel.DataType;
+import privacyModel.DocumentType;
 import privacyModel.LocationType;
+import privacyModel.Principal;
+import privacyModel.PrincipalScope;
+import privacyModel.PrincipalType;
 import privacyModel.ProcessingReason;
 import privacyModel.ProcessingReasonSubtype;
 import privacyModel.Purpose;
@@ -136,6 +143,48 @@ public class PrivacyDataFactory {
 			purpose.setProcessingReason(reason);
 			purpose.setProcessingReasonSubtype(subtype);
 			return purpose;
+		}
+		
+		public static void addChildCustodyDocument(String documentName, String childId, String parentId, Boolean shouldCreateParent)
+		{
+			var repo = new PrivacyModelRepository();
+			var model = repo.getModel();
+			var childObj = model.getAllPrincipals().stream().filter(principal -> principal.getName().equals(childId)).findFirst();
+			if(childObj.isEmpty())
+			{
+				System.out.println("There is no principal with id " + childId);
+				return;
+			}
+			Principal parent = null;
+			if(shouldCreateParent)
+			{
+				parent = repo.getFactory().createPrincipal();
+				parent.setName(parentId);
+				parent.setType(PrincipalType.NATURAL_PERSON);
+				parent.setScope(PrincipalScope.OUT);
+				parent.setAge(30);
+				childObj.get().getResponsiblePersons().add(parent);
+				model.getAllPrincipals().add(parent);
+				repo.saveModel(model);
+			}
+			else
+			{
+				var parentObj = model.getAllPrincipals().stream().filter(principal -> principal.getName().equals(parentId)).findFirst();
+				if(parentObj.isEmpty())
+				{
+					System.out.println("There is no principal with id " + parentId);
+					return;
+				}
+				parent = parentObj.get();
+			}
+			var documentObject = repo.getFactory().createDocument();
+			documentObject.setName(documentName);
+			documentObject.setDocumentType(DocumentType.CHILD_CUSTODY);
+			documentObject.setLocation("System");
+			documentObject.setProvidedBy(parent);
+			documentObject.setStartDate(Date.from(LocalDateTime.now().toInstant(ZoneOffset.UTC)));
+			model.getAllDocuments().add(documentObject);
+			repo.saveModel(model);
 		}
 	}
 }
